e9fd5bbb1b80fd15d2da6d9134fc8a0c69d239c0
Manoj Randeni <randeni.ramr@gmail.com>
2021-07-31 22:47:26 +0530

    Define Student class

--- src/lk/ijse/dep7/sms_lite/model/Student.java
+++ src/lk/ijse/dep7/sms_lite/model/Student.java
@@ -2,5 +2,40 @@ package lk.ijse.dep7.sms_lite.model;
 
 public class Student {
 
+    private String studentID;
+    private String name;
+    private String[] phoneNumbers;
 
+    public Student() {
+    }
+
+    public Student(String studentID, String name, String[] phoneNumbers) {
+        this.setStudentID(studentID);
+        this.setName(name);
+        this.setPhoneNumbers(phoneNumbers);
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumbers(String[] phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
 }
