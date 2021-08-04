489a3a9377df8e9ed3d915d6bfd12e7dfd6e64c3
Manoj Randeni <randeni.ramr@gmail.com>
2021-08-04 12:42:43 +0530

    Define MainFormController

--- src/lk/ijse/dep7/sms_lite/controller/MainFormController.java
+++ src/lk/ijse/dep7/sms_lite/controller/MainFormController.java
@@ -9,7 +9,13 @@ package lk.ijse.dep7.sms_lite.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainFormController {
     @FXML
@@ -18,10 +24,28 @@ public class MainFormController {
     private Button btnProviders;
 
     @FXML
-    private void btnSMS_onAction(ActionEvent event) {
+    private void btnSMS_onAction(ActionEvent event) throws IOException {
+        loadForm("../view/StudentManagementForm.fxml", "Student Management System");
+
     }
 
     @FXML
-    private void btnProviders_onAction(ActionEvent event) {
+    private void btnProviders_onAction(ActionEvent event) throws IOException {
+        loadForm("../view/ProviderForm.fxml", "Service Providers");
+    }
+
+
+    private void loadForm(String location, String title) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource(location));
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) btnSMS.getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.setTitle(title);
+        stage.setResizable(false);
+        stage.show();
+        stage.centerOnScreen();
     }
 }
