0b21a9932a0c5935e3c207014cfee403a7220315
Manoj Randeni <randeni.ramr@gmail.com>
2021-08-04 11:30:19 +0530

    Create HomeForm and Controller

---
+++ src/lk/ijse/dep7/sms_lite/controller/MainFormController.java
@@ -0,0 +1,27 @@
+/*
+ *
+ *  * Copyright (c) 2021 Manoj Randeni. All rights reserved.
+ *  * Licensed under the Apache license. See License.txt in the project root for license information
+ *
+ */
+
+package lk.ijse.dep7.sms_lite.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainFormController {
+    @FXML
+    private Button btnSMS;
+    @FXML
+    private Button btnProviders;
+
+    @FXML
+    private void btnSMS_onAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnProviders_onAction(ActionEvent event) {
+    }
+}
